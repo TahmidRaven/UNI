19096bcbdf379b3ecbb10be69556b509df1a1f1b
raven <raven.of.the.endless@gmail.com>
2023-06-21 00:09:50 +0600

    deleted files

--- CSE221o/task01a.py
+++
@@ -1,9 +0,0 @@
-with open("C:\\CODE\\TahmidRaven\\UNI\\CSE221o\\input01a.txt", "r") as input01a, open("output01a.txt", "w") as output01a:
-    t = int(input01a.readline())
-    for i in range(t):
-        i = int(input01a.readline())
-        if i%2 == 0 :
-            output01a.write(f"{i} is an EVEN number\n")
-        else: 
-            output01a.write(f"{i} is an ODD number\n")
-    print(t)
--- CSE221o/task03.py
+++
@@ -1,3 +0,0 @@
-with open("C:\\CODE\\TahmidRaven\\UNI\\CSE221o\\task03input01.txt", "r") as task03input01, open("C:\\CODE\\TahmidRaven\\UNI\\CSE221o\\task03output01.txt", "w") as task03output01:
-    times = int(task03input01.readline())
-    
