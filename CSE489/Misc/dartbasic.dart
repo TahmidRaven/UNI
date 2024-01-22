e86597d501188323775fc5d68bc525e52f16c3c6
raven <raven.of.the.endless@gmail.com>
2024-01-22 18:17:27 +0600

    CSE391 and CSE489 created dir

---
+++ CSE489/Misc/dartbasic.dart
@@ -0,0 +1,36 @@
+void main() {
+  // Numbers
+  int integerNumber = 42;
+  double doubleNumber = 3.14;
+
+  // Strings
+  String message = 'Hello, Dart!';
+
+  // Booleans
+  bool isDartFun = true;
+
+  // Lists
+  List<int> numbers = [1, 2, 3, 4, 5];
+  List<String> fruits = ['Apple', 'Banana', 'Orange'];
+
+  // Maps
+  Map<String, int> studentGrades = {'Alice': 90, 'Bob': 85, 'Charlie': 78};
+
+  // Sets
+  Set<int> uniqueNumbers = {1, 2, 3, 4, 5, 1, 2};
+
+  // Dynamic Type
+  dynamic dynamicVariable = 'This can hold any type';
+
+  // Print values
+  print('Integer: $integerNumber');
+  print('Double: $doubleNumber');
+  print('String: $message');
+  print('Boolean: $isDartFun');
+  print('Numbers: $numbers');
+  print('Fruits: $fruits');
+  print('Student Grades: $studentGrades');
+  print('Unique Numbers: $uniqueNumbers');
+  print('Dynamic Variable: $dynamicVariable');
+}
+
