a7b8eded4a61e7ab3f61ecd5cec94cbcbcef677c
raven <raven.of.the.endless@gmail.com>
2024-01-23 23:49:41 +0600

    I'm broke app

---
+++ CSE489/LearningFlutter/i_am_rich/android/app/src/main/kotlin/augustus_raven/com/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package augustus_raven.com.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ CSE489/LearningFlutter/i_am_rich/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+// void main() => runApp(MaterialApp(home: Center(child: Text('Hello'))));
+
+// the main fun is the starting point of all our flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text(
+            "I'm_Broke",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        // body: Center(
+        //   child: Image(
+        //     image: NetworkImage('https://i1.wp.com/bdn-data.s3.amazonaws.com/uploads/2019/12/32266281_H25797261.JPG?fit=1200%2C675&ssl=1'),
+        //   ),
+        // ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://i1.wp.com/bdn-data.s3.amazonaws.com/uploads/2019/12/32266281_H25797261.JPG?fit=1200%2C675&ssl=1'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
