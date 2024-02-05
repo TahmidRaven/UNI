6394c20aabbb06254ca8cbc61aafb9540818403e
raven <raven.of.the.endless@gmail.com>
2024-02-05 12:54:24 +0600

    finalized ass o1 project

--- CSE391/Assignments/Ass01/v1.01/js/resp.js
+++ CSE391/Assignments/Ass01/v1.01/js/resp.js
@@ -11,3 +11,22 @@ burger.addEventListener('click',() => {
 
 
 })
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var backToTopBtn = document.getElementById('backToTopBtn');
+
+    window.addEventListener('scroll', function () {
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 20) {
+            backToTopBtn.style.display = 'block';
+        } else {
+            backToTopBtn.style.display = 'none';
+        }
+    });
+
+    backToTopBtn.addEventListener('click', function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    });
+});
