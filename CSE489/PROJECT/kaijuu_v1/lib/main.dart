418a0b4a5ecbc2c886b0d51160708b67b74348ba
raven <raven.of.the.endless@gmail.com>
2024-04-05 00:43:50 +0600

    pomokaijuu firebase connection built

--- CSE489/PROJECT/kaijuu_v1/lib/main.dart
+++ CSE489/PROJECT/kaijuu_v1/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kaijuu_v1/firebase_options.dart';
 import 'homepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(MyApp());
 }
 
@@ -9,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pomodoro Kaijuu',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
